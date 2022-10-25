510e032bc4d3fe558858169d8ef8ca1b975ba3f0
Oleg O <komflat@yandex.ru>
2022-10-25 14:16:13 +0300

    Initial commit

---
+++ carsharing/common/Company.java
@@ -0,0 +1,39 @@
+package carsharing.common;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+    private int id;
+    private String name;
+
+    public Company(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
