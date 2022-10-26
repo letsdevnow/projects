8ec6ca8c4a8d423c1c1f2e3b9e713504f3ad5004
Oleg O <komflat@yandex.ru>
2022-10-26 15:09:32 +0300

    Commit

---
+++ cinema-rest-service/src/main/java/cinemarestservice/common/Response.java
@@ -0,0 +1,15 @@
+package cinemarestservice.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    @Getter
+    @Setter
+    private String error;
+    //@Getter @Setter private String debugMessage;
+}
