070ad01abe8847137546885059f36a2e75f48669
Oleg O <komflat@yandex.ru>
2022-10-26 14:56:45 +0300

    Initial commit

---
+++ cinema-rest-service/src/main/java/cinemarestservice/common/Seat.java
@@ -0,0 +1,29 @@
+package cinemarestservice.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+public class Seat {
+    private int row = 0;
+    private int column = 0;
+    @JsonIgnore
+    private boolean isBooked = false;
+    private int price = 0;
+    @JsonIgnore
+    private String token = "";
+
+    public Seat(int row, int column, int price) {
+        this.row = row;
+        this.column = column;
+        this.price = price;
+    }
+}
