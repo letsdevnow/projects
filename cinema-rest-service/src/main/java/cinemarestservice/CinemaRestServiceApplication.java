5ca92b23e3934843c93a70039c4dbd95fdebebba
Oleg O <komflat@yandex.ru>
2022-10-26 15:07:02 +0300

    1

---
+++ cinema-rest-service/src/main/java/cinemarestservice/CinemaRestServiceApplication.java
@@ -0,0 +1,13 @@
+package cinemarestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRestServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaRestServiceApplication.class, args);
+	}
+}
+
+
